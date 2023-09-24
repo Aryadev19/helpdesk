16d9de79b81ad4450d66f74c82d45894463c45ec
Piyush Aryaprakash <piyusharyaprakash12345@gmail.com>
2023-09-24 12:42:59 +0530

    initial commit

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h1 className="text-primary">Loading...</h1>
+        <p>Hopefully not for too long 😭</p>
+    </main>
+  )
+}
